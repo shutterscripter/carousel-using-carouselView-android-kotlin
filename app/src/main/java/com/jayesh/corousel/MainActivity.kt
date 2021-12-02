884c8ad5822a3e6bb6ee2df37ee800d411c90f88
Jay <Jay2272001@gmail.com>
2021-12-02 17:07:46 +0530

    added preview git

--- app/src/main/java/com/jayesh/corousel/MainActivity.kt
+++ app/src/main/java/com/jayesh/corousel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jayesh.corousel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.synnapps.carouselview.CarouselView
 import com.synnapps.carouselview.ImageListener
 
@@ -26,13 +27,17 @@ class MainActivity : AppCompatActivity() {
         cvHome = findViewById(R.id.cvHome)
         cvHome.pageCount = images.size
 
+        //setting images
         cvHome.setImageListener { position, imageView ->
             imageView.setImageResource(images[position])
         }
 
-//        cvHome.setImageClickListener { index ->
-//
-//        }
+        //onClickListener for images
+        cvHome.setImageClickListener { index -> //TODO: work to when user click on images
+            when(index){
+                0-> Toast.makeText(this, "image01", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
